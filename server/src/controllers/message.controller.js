1b0dfb94410ee6791a3a77a84123ff1653083563
Ashfaq Shimer <shimer.ahamed@gmail.com>
2025-08-28 15:01:19 +0530

    Develop route for sending and getting messages

--- server/src/controllers/message.controller.js
+++ server/src/controllers/message.controller.js
@@ -1,4 +1,7 @@
 import User from '../models/user.model.js';
+import Message from '../models/message.model.js';
+import cloudinary from '../utils/cloudinary.js';
+import ErrorResponse from '../utils/ErrorResponse.js';
 
 // @desc    Get all users to display on sidebar
 // @route   GET /api/v1/messages/users
@@ -13,4 +16,59 @@ export const getUsersForSidebar = async (req, res, next) => {
 		success: true,
 		data: filteredUsers,
 	});
+};
+
+// @desc    Display conversation with user
+// @route   GET /api/v1/messages/users
+// @access  Private
+export const getMessages = async (req, res, next) => {
+	const { _id: userToChatId } = req.params;
+
+	const myId = req.user._id;
+
+	const messages = await Message.find({
+		$or: [
+			{ senderId: myId, receiverId: userToChatId },
+			{ senderId: userToChatId, receiverId: myId },
+		],
+	});
+
+	res.status(200).json({
+		success: true,
+		data: messages,
+	});
+};
+
+// @desc    Send a message to a user
+// @route   POST /api/v1/messages/send/:id
+// @access  Private
+export const sendMessage = async (req, res, next) => {
+	const { text, image } = req.body ?? {};
+	if (!text && !image)
+		return next(new ErrorResponse("Please provide a 'text' or 'image'."));
+	const { id: receiverId } = req.params;
+	const senderId = req.user._id;
+
+	let imageUrl;
+	if (image) {
+		// Upload base64 image to cloudinary
+		const uploadResponse = await cloudinary.uploader.upload(image);
+		imageUrl = uploadResponse.secure_url;
+	}
+
+	const newMessage = new Message({
+		senderId,
+		receiverId,
+		text,
+		image: imageUrl,
+	});
+
+	await newMessage.save();
+
+	// const receiverSocketId = getReceiverSocketId(receiverId);
+	// if (receiverSocketId) {
+	// 	io.to(receiverSocketId).emit('newMessage', newMessage);
+	// }
+
+	res.status(201).json({ success: true, data: newMessage });
 };
--- server/src/routes/message.route.js
+++ server/src/routes/message.route.js
@@ -1,16 +1,15 @@
 import express from 'express';
 import { protect } from '../middleware/auth.middleware.js';
 import {
-	// getMessages,
+	getMessages,
 	getUsersForSidebar,
-	//   sendMessage
+	  sendMessage
 } from '../controllers/message.controller.js';
 
 const router = express.Router();
 
 router.get('/users', protect, getUsersForSidebar);
-// router.get("/:id", protect, getMessages);
-
-// router.post("/send/:id", protect, sendMessage);
+router.get("/:id", protect, getMessages);
+router.post("/send/:id", protect, sendMessage);
 
 export default router;
