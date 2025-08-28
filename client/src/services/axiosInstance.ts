05a216910186fd63a9564dae6aaa09c9a545500c
Ashfaq Shimer <shimer.ahamed@gmail.com>
2025-08-28 16:36:44 +0530

    Setup client

---
+++ client/src/services/axiosInstance.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+	baseURL:
+		import.meta.env.MODE === 'development'
+			? 'http://localhost:5001/api'
+			: import.meta.env.VITE_API_URL,
+	withCredentials: true,
+});
+
+// Add a request interceptor
+axiosInstance.interceptors.request.use(
+	(config) => {
+		// Get the token from localStorage or wherever it's stored
+		const token = localStorage.getItem('token');
+
+		// If a token exists, attach it to the headers
+		if (token) {
+			config.headers.Authorization = `Bearer ${JSON.parse(token)}`;
+		}
+
+		return config;
+	},
+	(error) => {
+		return Promise.reject(error);
+	}
+);
+
+export default axiosInstance;
